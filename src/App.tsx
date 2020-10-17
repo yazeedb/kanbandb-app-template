314c27f62748665dc71cf147bda0a5914acdd057
Yazeed Bzadough <ybzadough@gmail.com>
2020-10-17 15:43:56 -0400

    strip logic from UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,8 @@
 import React from 'react';
 import './App.css';
-import KanbanDB from 'kanbandb/dist/KanbanDB';
 
-function initialize() {
-  /**
-   *
-   * Use KanbanDB like so (but you might want to move it) - types are provided:
-   *
-   */
-
-  KanbanDB.connect('');
-}
-
-function App() {
-  // Initialize DB communications.
-  initialize();
-
+const App = () => {
   return <main></main>;
-}
+};
 
 export default App;

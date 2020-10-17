7ba2ca065d780f2e5d4c0ff5a48bb536d6ea84f3
Yazeed Bzadough <ybzadough@gmail.com>
2020-10-17 16:24:23 -0400

    render columns and add styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,49 @@
 import React from 'react';
-import './App.css';
+import { useMachine } from '@xstate/react';
+import { FaEdit, FaTrash } from 'react-icons/fa';
+import { boardMachine } from './boardMachine';
+import './App.scss';
 
 const App = () => {
-  return <main></main>;
+  const [{ matches, context }, send] = useMachine(boardMachine, {
+    devTools: true
+  });
+
+  return (
+    <main>
+      <section className="columns">
+        {context.columns.map((column) => (
+          <div className="column">
+            <header className="column-header">
+              <h2>{column.name}</h2>
+            </header>
+
+            <section className="column-body">
+              {column.cards.map((card) => (
+                <div className="card">
+                  <header className="card-header">
+                    <span className="name">{card.name}</span>
+
+                    <button className="edit-card">
+                      <FaEdit />
+                    </button>
+
+                    <button className="delete-card">
+                      <FaTrash />
+                    </button>
+                  </header>
+
+                  <section className="card-description">
+                    {card.description}
+                  </section>
+                </div>
+              ))}
+            </section>
+          </div>
+        ))}
+      </section>
+    </main>
+  );
 };
 
 export default App;

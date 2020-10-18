779739db91971126d932b25f55e100f1f07f64a5
Yazeed Bzadough <ybzadough@gmail.com>
2020-10-17 22:16:00 -0400

    use rsuite dark theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'rsuite/dist/styles/rsuite-default.css';
+import 'rsuite/dist/styles/rsuite-dark.css';
 import App from './App';
 
 ReactDOM.render(
